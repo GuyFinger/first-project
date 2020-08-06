16f8d38d814ebc038d6864c73cf5bbfc172e3543
Guy Finger <68593925+GuyFinger@users.noreply.github.com>
2020-08-06 22:13:45 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,39 +1,80 @@
+////////////////////////////////////////////////////////////////////////////          ↓↓↓ prevent printing note if inputs empty. else, print note ↓↓↓
+
 function preventNote() {
   if (form.elements["title"].value == "") {
     alert("Title field empty!");
+    return true;
   } else if (form.elements["Text1"].value == "") {
     alert("Text Area field empty!");
+    return true;
   } else if (form.elements["date"].value == "") {
     alert("Date field empty!");
+    return true;
   } else {
-    addTask();
+    return false;
   }
 }
+////////////////////////////////////////////////////////////////////////////          ↓↓↓ define limit of scroll to show page up button ↓↓↓
 
 function scrollFunction() {
   if (
     document.body.scrollTop > 600 ||
     document.documentElement.scrollTop > 600
   ) {
-    mybutton.style.display = "block";
+    scrollBackToTop.style.display = "block";
   } else {
-    mybutton.style.display = "none";
+    scrollBackToTop.style.display = "none";
   }
 }
+////////////////////////////////////////////////////////////////////////////          ↓↓↓ define page up ↓↓↓
 
 function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+////////////////////////////////////////////////////////////////////////////          ↓↓↓ remove tab from DOM ↓↓↓
 
 function closetab(e) {
-  x = e.target.parentElement;
+  noteCloseEvent = e.target.parentElement;
   // x.style.display = "none";
-  x.remove();
+  noteCloseEvent.remove();
+  return noteCloseEvent;
+}
+////////////////////////////////////////////////////////////////////////////          ↓↓↓ Remove tab from Local Storage ↓↓↓
+function removeTabFromLocalStorage(e) {
+  let notes = JSON.parse(window.localStorage.getItem("notes"));
+  for (let removenote = 0; removenote < notes.length; removenote++) {
+    if (notes[removenote].id == e.target.id) {
+      notes.splice(removenote, 1);
+      window.localStorage.setItem("notes", JSON.stringify(notes));
+    }
+  }
+}
+////////////////////////////////////////////////////////////////////////////          ↓↓↓ note counter for note and local storage ID cross ↓↓↓
+
+function notesCounter() {
+  counter++;
+  console.log(counter);
+  return counter;
+}
+function saveCounterToLocalStorage(counter) {
+  JSON.parse(window.localStorage.getItem("counter"));
+  window.localStorage.setItem("counter", JSON.stringify(counter));
 }
 
-function addTask() {
-  ////////////////////////////////////////////////////////////////////      get values from inputs
+////////////////////////////////////////////////////////////////////////////          ↓↓↓ create and add note to DOM and local storage ↓↓↓
+
+function addNoteOnClick() {
+  if (!preventNote()) {
+    notesCounter(counter);
+    saveCounterToLocalStorage(counter);
+    let newNote = getValFromDOM();
+    saveNoteValuesToLocalStorage(newNote);
+    createNote(newNote);
+  }
+}
+////////////////////////////////////////////////////////////////////////////          ↓↓↓ get values from inputs ↓↓↓
+function getValFromDOM() {
   let title = document.getElementById("title").value;
 
   let disc = document.getElementById("textarea").value;
@@ -42,23 +83,37 @@ function addTask() {
 
   let hour = document.getElementById("hour").value;
 
-  ///////////////////////////////////////////////////////////////////       reverse Date
-  function reverseDate() {
-    let reverseDate = date;
-    return reverseDate.split("-").reverse().join("-");
-  }
-  ////////////////////////////////////////////////////////////////////     create new sticky note
+  let id = counter;
 
+  return { title, disc, date, hour, id };
+}
+////////////////////////////////////////////////////////////////////////////          ↓↓↓ save note values into local storage ↓↓↓
+
+function saveNoteValuesToLocalStorage(newNote) {
+  let notes = JSON.parse(window.localStorage.getItem("notes"));
+  notes.push(newNote);
+  window.localStorage.setItem("notes", JSON.stringify(notes));
+}
+
+////////////////////////////////////////////////////////////////////////////          ↓↓↓ reverse Date ↓↓↓
+
+function reverseDate(date) {
+  let reverseDate = date;
+  return reverseDate.split("-").reverse().join("-");
+}
+////////////////////////////////////////////////////////////////////////////          ↓↓↓ create new sticky note ↓↓↓
+
+function createNote(newNote) {
   //time div
   let parDate = document.createElement("p");
   parDate.setAttribute("class", "lead");
   parDate.setAttribute("class", "logdate");
-  parDate.textContent = reverseDate();
+  parDate.textContent = reverseDate(newNote.date);
 
   let parHour = document.createElement("p");
   parHour.setAttribute("class", "lead");
   parHour.setAttribute("class", "logtime");
-  parHour.textContent = hour;
+  parHour.textContent = newNote.hour;
 
   let divTime = document.createElement("div");
   divTime.setAttribute("class", "time");
@@ -69,12 +124,12 @@ function addTask() {
   let parDisc = document.createElement("p");
   parDisc.setAttribute("class", "lead");
   parDisc.setAttribute("class", "notedisc");
-  parDisc.textContent = disc;
+  parDisc.textContent = newNote.disc;
 
   let headTitle = document.createElement("h5");
-  headTitle.setAttribute("class", "display-5");
+  headTitle.setAttribute("class", "display-4");
   headTitle.setAttribute("class", "notetitle");
-  headTitle.textContent = title;
+  headTitle.textContent = newNote.title;
 
   let divNotecontent = document.createElement("div");
   divNotecontent.setAttribute("class", "notewords");
@@ -82,25 +137,52 @@ function addTask() {
   divNotecontent.appendChild(parDisc);
 
   //create X with font awesome to close tab
-  let span = document.createElement("i");
-  span.setAttribute("class", "closex fas fa-times fa-2x");
-  span.addEventListener("click", closetab);
+  let closeTabIcon = document.createElement("i");
+  closeTabIcon.setAttribute("class", "closex fas fa-times fa-2x");
+  closeTabIcon.setAttribute("id", counter);
+  closeTabIcon.style = "cursor: pointer";
+  closeTabIcon.id;
+
+  closeTabIcon.addEventListener("click", closetab);
+  closeTabIcon.addEventListener("click", removeTabFromLocalStorage);
 
   //card div
   let divNote = document.createElement("div");
   divNote.setAttribute("class", "note");
   divNote.appendChild(divNotecontent);
   divNote.appendChild(divTime);
-  divNote.appendChild(span);
+  divNote.appendChild(closeTabIcon);
 
   let divNotecontainer = document.getElementById("noteContainer");
   divNotecontainer.appendChild(divNote);
 }
-///////////////////////////////////////////////////////////////////////////////////////
-mybutton = document.getElementById("pageup");
+//FUNCTIONS ABOVE
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//CODE BELOW
 
+let counter;
+// check if local storage counter is empty. if so, create counter key.
+if (localStorage.getItem("counter") === null) {
+  counter = 0;
+  window.localStorage.setItem("counter", JSON.stringify([]));
+}
+
+// check if local storage is empty. if not, get data from local storage to DOM
+if (localStorage.getItem("notes") === null) {
+  window.localStorage.setItem("notes", JSON.stringify([]));
+} else {
+  let fetchNotes = JSON.parse(window.localStorage.getItem("notes"));
+
+  // for (let i = 0; i < fetchNotes.length; i++) {
+  //   createNote(fetchNotes[i]);
+  // }
+  for (const newNote of fetchNotes) {
+    createNote(newNote);
+  }
+}
+scrollBackToTop = document.getElementById("pageup");
 window.onscroll = function () {
   scrollFunction();
 };
-document.getElementById("add").addEventListener("click", preventNote);
+document.getElementById("add").addEventListener("click", addNoteOnClick);
 document.getElementById("pageup").addEventListener("click", topFunction);
